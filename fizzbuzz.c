dc13ee5cdb630943d2c327700b0948f59363c36c
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2016-03-13 18:08:33 +0900

    first

---
+++ fizzbuzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int a = 1;
+
+while(a<=100)
+{
+if(a%15==0) printf("FizzBuzz ");
+else if(a%3==0) printf("Fizz ");
+else if(a%5==0) printf("Buzz ");
+else printf("%d ", a);
+a++;
+if(a%10==0) printf("\n");
+}
+return 0;
+}
