5a214bdf87eb36b059e3c2f3e2c9fd189354ea36
ioshin <chikke.black@gmail.com>
2016-03-13 18:34:22 +0900

    refs #1 2,3,5の倍数でgithubと表示

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -6,7 +6,8 @@ int a = 1;
 
 while(a<=100)
 {
-if(a%15==0) printf("FizzBuzz ");
+  if(a%30==0) printf("GitHub ");
+ else if(a%15==0) printf("FizzBuzz ");
 else if(a%3==0) printf("Fizz ");
 else if(a%5==0) printf("Buzz ");
 else printf("%d ", a);
